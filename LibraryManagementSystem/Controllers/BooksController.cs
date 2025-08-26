a9b7bd4965f53ef1dd84835ae9b4b736f0f663b7
User <User@DESKTOP-GAB3BFD>
2025-08-26 15:51:41 +0200

    Add project files.

---
+++ LibraryManagementSystem/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
